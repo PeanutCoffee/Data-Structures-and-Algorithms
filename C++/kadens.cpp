2ea7c1c75447f1c37fb81b595f89026f8b47f71d
Yash Gupta <hpsyash@gmail.com>
2022-10-01 10:26:47 +0530

    Create kadens.cpp

---
+++ C++/kadens.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxSubarraySum(int arr[], int n){
+    
+    // Your code here
+    int max_so_far = INT_MIN, max_ending_here = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        max_ending_here = max_ending_here + arr[i];
+        if (max_so_far < max_ending_here)
+            max_so_far = max_ending_here;
+
+        if (max_ending_here < 0)
+            max_ending_here = 0;
+    }
+    return max_so_far;
+    
+}
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+        
+        cout << maxSubarraySum(a, n) << endl;
+    }
+}
