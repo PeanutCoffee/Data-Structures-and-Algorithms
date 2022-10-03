4724adb8e8c6a0d5fffe5b142b793bb9fa7d92be
Yash Gupta <hpsyash@gmail.com>
2022-10-03 23:17:14 +0530

    Added MATLAB Codes

--- MATLAB Codes/exp7.m
+++ MATLAB Codes/exp7.m
@@ -1,43 +1,52 @@
-clc;
 clear all;
 close all;
-n = input('Number of Symbols:');
-p = zeros(1, n);
-for i=1:n
-fprintf('Give the probability for %dth symbol:', i);
-p(i) = input('');
-end
-[p, I] = sort(p, 'descend');
-codes = getCodes(p);
-Lk_pk = zeros(1, n);
-for i=1:length(codes)
-fprintf('\nCode for %dth Symbol: ', I(i));
-fprintf('%g ', codes{i});
-fprintf('\n');
-Lk_pk(i) = length(codes{i}) * p(i);
-end
-avg_L = sum(Lk_pk);
-Entropy = 0;
-for i=1:n
-Entropy = Entropy + (-1 * p(i) * log2(p(i)));
-end
-fprintf('\nEntropy: %f\n', Entropy);
-efficiency = (Entropy / avg_L) * 100;
-fprintf('\nEfficiency: %f\n', efficiency);
-function codes=getCodes(p)
-if (length(p) == 2)
-codes = {[0]; [1]};
-else
-len = length(p);
-p_new = [p(1:len-2), p(len - 1) + p(len)];
-p_new = sort(p_new, 'descend');
-codes = getCodes(p_new);
-codeLen = length(codes);
-pos = find(p_new==(p(len - 1) + p(len)));
-pos = pos(1);
-codes = [codes(1:pos - 1); codes(pos + 1:codeLen); codes(pos); codes(pos)];
-codeLen = length(codes);
-codes{codeLen} = [codes{codeLen}, 1];
-codes{codeLen - 1} = [codes{codeLen - 1}, 0];
-end
-end
+clc;
+
+Fp=2000; %Fp=2KHz 
+Fs=5000; %Fs=5KHz 
+Ft=20000; %Ft=20KHz 
+wp=(2*pi*Fp)/Ft; 
+ws=(2*pi*Fs)/Ft; 
+trwidth=ws-wp; %Transition Width 
+
+M=ceil(6.2*pi/trwidth); %FilterLength
+tau=(((M-1)/2))
+wc=(wp+ws)/2;    %cutoff frequency
+n=0:M-1; 
+hd=(sin(wc*(n-tau)))./(pi*(n-tau)); %desired filter
+hd(tau+1)=0.35;  %found using l'hopital's rule manually
+whan=hann(M); 
+whan=whan.';   %converting row to column matrix as to be multiplied later
+h=hd.*whan;  %practical filter impulse response after multiplying with window
+w=0:0.02:pi; 
+Hw=freqz(h,1,w);    
+MagHw=abs(Hw); %Magnitude Response  
+HwdB=20*log(MagHw);
+
+subplot(2,2,1); 
+stem(n,hd,'filled'); 
+axis([-1 M -0.15 0.5]); 
+xlabel('n'); 
+ylabel('hd(n)'); 
+title('Ideal Impulse Response'); 
+
+subplot(2,2,2); 
+stem(n,whan,'filled'); 
+axis([-1 M -0.1 1.2]); 
+xlabel('n'); 
+ylabel('w(n)'); 
+title('Hann Window'); 
+
+subplot(2,2,3); 
+stem(n,h,'filled'); 
+axis([-1 M -0.15 0.5]); 
+xlabel('n'); 
+ylabel('w(n)'); 
+title('Practical Impulse Response'); 
+
+subplot(2,2,4); 
+plot(w/pi,HwdB); 
+axis([0 1 -100 10]);        
+xlabel('Frequency in pi '); 
+ylabel('dB'); 
+title('Magnitude Response');
