38363a46e8b6be7804a20dafc15d6b88697e813f
Yash Gupta <hpsyash@gmail.com>
2022-10-03 23:17:06 +0530

    Added MATLAB Codes

--- MATLAB Codes/exp5.m
+++ MATLAB Codes/exp5.m
@@ -1,75 +1,36 @@
-clc;
-clear all;
-close all;
-%Speech Sample CDF and PDF Plot
-fileName = ('sing.wav'); % Importing The Speech File
-[audioIn_Sample, fs] = audioread(fileName);
-info = audioinfo(fileName);
-audioIn_Sample = audioIn_Sample';
-A = max(audioIn_Sample);
-miu = 255;
-u_law = ((log(1 + (abs(audioIn_Sample) ./ A) .* miu)) ./ log(1 + miu));
-[M, N] = size(u_law);
-L = 16;
-%Compressing using miu-law
-delta = (max(u_law) - min(u_law)) / L;
-non_uniform_qs = zeros(1, N);
-%Quantization
-for i=1:N
-start = min(u_law);
-while(start < u_law(i))
-start = start + delta;
+n=0:99;
+k=1:100;
+x=cos(0.48*pi*n)+cos(0.52*pi*n);
+xpad=[x(1:10),zeros(1,90)];  %first input signal with 90padded zeros
+N=100;
+w=2*pi/N;
+DFTpadded=zeros(1,100);
+for l=1:100            %kth sample of DFT is l here
+    for i=1:100
+    a=xpad(i)*exp(-j*w*(i-1)*l);
+    DFTpadded(l)=DFTpadded(l)+a;
+    end
 end
-non_uniform_qs(i) = start - delta;
-if (start == u_law(i))
-non_uniform_qs(i) = start;
+
+xoriginal=x(1:100);
+DFToriginal=zeros(1,100);
+for l=1:100            %kth sample of DFT is l here
+    for i=1:100
+    a=xoriginal(i)*exp(-j*w*(i-1)*l);
+    DFToriginal(l)=DFToriginal(l)+a;
+    end
 end
-end
-%Uniform Quantization
-delta_uniform = (max(audioIn_Sample) - min(audioIn_Sample)) / L;
-uniform_qs = zeros(1, N);
-for i=1:N
-start = min(audioIn_Sample);
-while(start < audioIn_Sample(i))
-start = start + delta_uniform;
-end
-uniform_qs(i) = start - delta_uniform;
-if (start == audioIn_Sample(i))
-uniform_qs(i) = start;
-end
-end
-error_uniform = uniform_qs - audioIn_Sample;
-error_non_uniform = non_uniform_qs - u_law;
-speech_signal_power = mean(audioIn_Sample .^ 2);
-u_law_power = mean(u_law .^ 2);
-uniform_quantization_power = mean(error_uniform .^ 2);
-non_uniform_quantization_power = mean(error_non_uniform .^ 2);
-snr_uniform = speech_signal_power / uniform_quantization_power;
-snr_non_uniform = u_law_power / non_uniform_quantization_power;
-fprintf('Output SNR for Uniform Quantizer: %f\n', snr_uniform);
-fprintf('\nOutput SNR for Non-Uniform Quantizer: %f\n', snr_non_uniform);
-figure(1);
-subplot(3,1,1);
-plot(audioIn_Sample);
-xlabel('Time');
-ylabel('Original Speech Signal');
-figure(1);
-subplot(3,1,2);
-plot(uniform_qs);
-xlabel('Time');
-ylabel('Uniform Quantized Speech Signal');
-figure(1);
-subplot(3,1,3);
-plot(non_uniform_qs);
-xlabel('Time');
-ylabel('Non Uniform Quantized Speech Sample');
-figure(2);
-subplot(1,2,1);
-plot(error_uniform);
-xlabel('Time');
-ylabel('Uniform Quantization Error');
-figure(2);
-subplot(1,2,2);
-plot(error_non_uniform);
-xlabel('Time');
-ylabel('Non Uniform Quantization Error');
+
+
+subplot(2,2,1);
+stem(x);
+title("Original signal ");
+subplot(2,2,2);
+stem(DFToriginal);
+title("DFT of original signal");
+subplot(2,2,3);
+stem(xpad);
+title("Zero padded input signal");
+subplot(2,2,4);
+stem(DFTpadded);
+title("DFT of zero padded signal");
